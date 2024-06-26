619b6e243ccc08d27de913ab847117f5044ef35c
benjaminhernandez1981 <benjamineric2005@gmail.com>
2024-06-25 20:42:24 -0500

    react-crash-course-wk5

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import "./App.css";
-import Todo from "./components/Todo.jsx";
-import Title from "./components/Title.jsx";
 
 function App() {
   return (
     <div>
-      <Title />
-      <div className="todo_wrapper">
-        <Todo />
-        <Todo />
-        <Todo />
+      <h1>My Todo List</h1>
+      <div>
+        <h2>Finish Frontend Simplified</h2>
+        <button>Delete</button>
+      </div>
+      <div>
+        <h2>Finish the interview section</h2>
+        <button>Delete</button>
+      </div>
+      <div>
+        <h2>Land $100k Job</h2>
+        <button>Delete</button>
       </div>
     </div>
   );
