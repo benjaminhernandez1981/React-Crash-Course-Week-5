ff9dcd3204cf0b33a21c5b02481b9b065209a6f5
benjaminhernandez1981 <benjamineric2005@gmail.com>
2024-07-03 21:17:49 -0500

    react-crash-course-wk5

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,20 @@
 import "./App.css";
-import Todo from "./components/Todo.jsx";
-import Title from "./components/Title.jsx";
-import Modal from "./components/Modal.jsx";
-import Counter from "./components/Counter.jsx";
-import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Contact from "./pages/Contact.jsx";
+import Nav from "./components/Nav.jsx";
+import Users from "./pages/Users.jsx";
 
 function App() {
-  const [showModal, setShowModal] = useState(false);
-
-  function onTodoDelete() {
-    setShowModal(true);
-  }
-
-  function cancelModal() {
-    setShowModal(false);
-  }
-
-  function confirmModal() {
-    setShowModal(false);
-  }
-
-  useEffect(() => {
-    console.log("ONLY on mount");
-    setShowModal(!showModal);
-  }, []);
-
-  useEffect(() => {
-    console.log(`on mount AND on ${showModal} change`);
-  }, [showModal]);
-
-  useEffect(() => {
-    console.log("EVERY render");
-  });
-
   return (
-    <div>
-      <Title />
-      <div>
-        <input
-          type="text"
-          onChange={(event) => {
-            console.log(event.target.value);
-          }}
-        />
-        <button onClick={() => setShowModal(true)}>Add todo</button>
-      </div>
-      <div className="todo__wrapper">
-        <Todo onTodoDelete={onTodoDelete} title="Finish Frontend Simplified" />
-        <Todo onTodoDelete={onTodoDelete} title="Finish Interview Section" />
-        <Todo
-          onTodoDelete={onTodoDelete}
-          title="Land a $100k Job"
-          paragraph="Apply to 100 jobs"
-        />
-      </div>
-      {showModal && (
-        <Modal
-          cancelModal={cancelModal}
-          confirmModal={confirmModal}
-          title="Confirm delete?"
-        />
-      )}
-    </div>
+    <Router>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/users/:username" element={<Users />} />
+      </Routes>
+    </Router>
   );
 }
 
