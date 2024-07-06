fda4f2ea5bab5e69993ff1f03e8d18679fa3796f
benjaminhernandez1981 <benjamineric2005@gmail.com>
2024-07-06 18:34:23 -0500

    react-crash-course-wk5

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function Home() {
-  const [users, setUsers] = useState([]);
+ const [users, setUsers] = useState([]) 
 
   async function fetchUsers() {
     const { data } = await axios.get(
@@ -14,7 +14,7 @@ function Home() {
   useEffect(() => {
     fetchUsers();
   }, []);
-  return <h1>{users[0].name}</h1>;
+  return <h1>{users.length > 0 ? users[0].name : null}</h1>;
 }
 
 export default Home;
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,10 +1,9 @@
 import { useParams } from "react-router-dom";
-import React, { useEffect } from "react";
 
 function Users() {
-  const { username } = useParams();
+  const { id } = useParams();
 
-  return <h1>{username}</h1>;
+  return <h1>{id}</h1>;
 }
 
 export default Users;
