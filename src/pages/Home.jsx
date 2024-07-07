0a91d39cb1f570cc397669e0735f6e3841b48225
benjaminhernandez1981 <benjamineric2005@gmail.com>
2024-07-07 01:50:25 -0500

    react-crash-course-wk5

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import User from "../components/User.jsx";
+import { Link } from "react-router-dom";
 
 function Home() {
   const [users, setUsers] = useState([]);
@@ -13,22 +15,21 @@ function Home() {
   }
 
   useEffect(() => {
-    setTimeout(() => {
-      fetchUsers();
-    }, 500);
-  }, []);
-
-  const pixels = "3px";
+         fetchUsers();
+      }, []);
 
   return (
     <div>
       {users.map((user) => (
-          <div style={{ border: `${pixels} solid black` }}>
-            <div>{user?.id}</div>
-            <div>{user?.name}</div>
-            <div>{user?.email}</div>
-            <div>{user?.username}</div>
-          </div>
+        <Link to={`/users/${user.id}`} key={user.id}>
+        <User
+        key={user.id}
+          id={user.id}
+          name={user.name}
+          email={user.email}
+          username={user.username}
+        />
+        </Link>
       ))}
     </div>
   );
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,9 +1,30 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function Users() {
   const { id } = useParams();
+  const [user, setUser] = useState({});
 
-  return <h1>{id}</h1>;
+  async function fetchUser() {
+    const { data } = await axios.get(
+      `https:jsonplaceholder.typicode.com/users/${id}`
+    );
+    setUser(data);
+  }
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+  return (
+    <div>
+      <Link to="/">Go back</Link>
+      <h1>{user.id}</h1>
+      <h1>{user.name}</h1>
+      <h1>{user.email}</h1>
+      <h1>{user.username}</h1>
+    </div>
+  );
 }
 
 export default Users;
