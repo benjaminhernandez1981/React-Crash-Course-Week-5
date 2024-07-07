fd3eb8bd4f9f9862d55f19223d6601c37a299ed7
benjaminhernandez1981 <benjamineric2005@gmail.com>
2024-07-07 01:07:48 -0500

    react-creash-course-wk5

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
       "https://jsonplaceholder.typicode.com/users"
     );
     setUsers(data);
-    console.log(data)
+    console.log(data);
   }
 
   useEffect(() => {
@@ -17,13 +17,23 @@ function Home() {
       fetchUsers();
     }, 500);
   }, []);
-  const pixels = "2px";
-  return;
-  <div style={{ border: `${pixels} solid black` }}>
-    <div>Id</div>
-    <div>Name</div>
-    <div>Email</div>
-    <div>Username</div>
-  </div>;
+
+  const pixels = "3px";
+
+  return (
+    <div>
+      {users.map((user) => {
+        return (
+          <div style={{ border: `${pixels} solid black` }}>
+            <div>{user?.id}</div>
+            <div>{user?.name}</div>
+            <div>{user?.email}</div>
+            <div>{user?.username}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
+
 export default Home;
